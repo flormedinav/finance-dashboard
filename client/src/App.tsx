ca398ba2065ede3ebbd700abcc09053a9e61a5e3
flormedinav <flormedinav7@gmail.com>
2024-03-12 20:22:25 -0300

    add base config added

---
+++ client/src/App.tsx
@@ -0,0 +1,37 @@
+import { useMemo } from "react";
+import { createTheme } from "@mui/material/styles";
+import { Box, CssBaseline, ThemeProvider } from "@mui/material";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+import { themeSettings } from "./theme";
+import { Dashboard, NavBar } from "@/scenes";
+import { GLOBAL_CONSTANTS } from "@/constants/global";
+
+function App() {
+  const theme = useMemo(() => createTheme(themeSettings), []);
+
+  return (
+    <div className="app">
+      <BrowserRouter>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <Box width="100%" height="100%" padding="1rem 2rem 4rem 2rem">
+            <NavBar />
+            <Routes>
+              <Route
+                path={GLOBAL_CONSTANTS.APP_URL.DASHBOARD}
+                element={<Dashboard />}
+              />
+              <Route
+                path={GLOBAL_CONSTANTS.APP_URL.PREDICTIONS}
+                element={<div>Prediction</div>}
+              />
+            </Routes>
+          </Box>
+        </ThemeProvider>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
